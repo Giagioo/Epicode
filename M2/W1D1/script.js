2b3ff56bdc5576fcb77fe0f9a8ceb6d253133d03
Giacomo Gomez Val <giacomogomezval98@gmail.com>
2024-03-13 13:13:49 +0100

    .

---
+++ M2/W1D1/script.js
@@ -0,0 +1,17 @@
+let miglia
+const CONVERSIONE = 1.609
+let chilometri
+
+miglia = 50
+chilometri = miglia * CONVERSIONE
+
+console.log("Il risultato è" + chilometri)
+
+//document.write("Il risultato è" + chilometri)
+
+
+
+
+
+
+
