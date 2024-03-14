110b5b57cb561cb095efffd43cb9acba09ee88b3
Giacomo Gomez Val <giacomogomezval98@gmail.com>
2024-03-14 21:31:38 +0100

    lezione W1D4

--- M2/W1D1/script.js
+++ M2/W1D1/script.js
@@ -1,18 +1,7 @@
-let miglia
-const CONVERSIONE = 1.609
-let chilometri
+let farenheit = 55
+let gradi
+const rapporto = 0.555
+const differenza = 32
 
-miglia = 50
-chilometri = miglia * CONVERSIONE
-
-console.log("Il risultato è" + chilometri)
-
-//document.write("Il risultato è" + chilometri)
-
-//prova push
-
-
-
-
-
-nkml
+gradi = (farenheit - differenza) * rapporto
+document.write(`Secondo questa conversione i gradi sono ${gradi} `)
